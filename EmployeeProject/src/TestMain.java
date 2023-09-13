ca830a4cb775c361e5b661e17ed3b19fffd9c133
Kau4r <22102606@usc.edu.ph>
2023-09-13 13:33:13 +0800

    Used Base plus Commission

--- EmployeeProject/src/TestMain.java
+++ EmployeeProject/src/TestMain.java
@@ -4,7 +4,7 @@ import version2.Employee;
 
 public class TestMain {
     public static void main(String[] args) {
-        Employee.Commission julz=new Employee.Commission(22102606 , "Julz", LocalDate.of(2003, 9, 24), LocalDate.now(), 400000);
+        Employee.Commission.Base julz=new Employee.Commission.Base(22102606 , "Julz", LocalDate.of(2003, 9, 24), LocalDate.now(), 400000,50000);
         julz.displayInfo();
     }
 }
